90cc6cf2d84f899dd694d1d6805abf0f9b3d3402
Jon Ringvald <jvald8@gmail.com>
2015-06-15 11:51:02 -0700

    adding users, columns, boards, cards resources to server

--- src/server.js
+++ src/server.js
@@ -23,30 +23,148 @@ var Board = new PersistentResource(mongoURL,
     });
 
     app.use(bodyParser.json());
-    app.post('/todos', function(req, res) {
-      var hash = { name: 'default name' };
+
+    app.post('/boards', function(req, res) {
+      var hash = { name: 'default board name' };
       Resource.create(hash, function(err, doc) {
         if (err) { res.status(500).send('Database Error'); }
         else { res.status(201).send(doc._doc); }
       });
     });
 
-    app.put('/todos/:id', function(req, res) {
+    app.put('/boards/:id', function(req, res) {
       Resource.save(req.params.id, req.body, function(err, doc) {
         if (err) { res.status(500).send('Database Error'); }
-        else { res.status(200).send('Updated resource ' + doc._doc._id); }
+        else { res.status(200).send('Updated board ' + doc._doc._id); }
       });
     });
 
-    app.delete('/todos/:id', function(req, res) {
+    app.delete('/boards/:id', function(req, res) {
       Resource.drop(req.params.id, function(err) {
         if (err) { res.status(500).send('Database Error'); }
         else { res.status(200).send('Dropped resource ' + req.params.id); }
       });
     });
+  });
 
-    var port = 3000;
-    app.listen(port, function() {
-      console.log('Listening on :' + port + '.');
+var Column = new PersistentResource(mongoURL,
+  'yelo', 'columns', {
+    name: String,
+  },
+  function(err) {
+    if (err) { throw new Error('ERROR: Could not connect to MongoDB'); }
+
+    app.get('/columns', function(req, res) {
+      Resource.getAll(function(err, docs) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(200).send(docs); }
+      });
+    });
+
+    app.use(bodyParser.json());
+
+    app.post('/columns', function(req, res) {
+      var hash = { name: 'default column name' };
+      Resource.create(hash, function(err, doc) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(201).send(doc._doc); }
+      });
+    });
+
+    app.put('/columns/:id', function(req, res) {
+      Resource.save(req.params.id, req.body, function(err, doc) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(200).send('Updated board ' + doc._doc._id); }
+      });
+    });
+
+    app.delete('/columns/:id', function(req, res) {
+      Resource.drop(req.params.id, function(err) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(200).send('Dropped resource ' + req.params.id); }
+      });
     });
   });
+
+var User = new PersistentResource(mongoURL,
+  'yelo', 'users', {
+    name: String,
+  },
+  function(err) {
+    if (err) { throw new Error('ERROR: Could not connect to MongoDB'); }
+
+    app.get('/users', function(req, res) {
+      Resource.getAll(function(err, docs) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(200).send(docs); }
+      });
+    });
+
+    app.use(bodyParser.json());
+
+    app.post('/users', function(req, res) {
+      var hash = { name: 'default user name' };
+      Resource.create(hash, function(err, doc) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(201).send(doc._doc); }
+      });
+    });
+
+    app.put('/users/:id', function(req, res) {
+      Resource.save(req.params.id, req.body, function(err, doc) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(200).send('Updated board ' + doc._doc._id); }
+      });
+    });
+
+    app.delete('/users/:id', function(req, res) {
+      Resource.drop(req.params.id, function(err) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(200).send('Dropped resource ' + req.params.id); }
+      });
+    });
+  });
+
+var Card = new PersistentResource(mongoURL,
+  'yelo', 'cards', {
+    name: String,
+  },
+  function(err) {
+    if (err) { throw new Error('ERROR: Could not connect to MongoDB'); }
+
+    app.get('/cards', function(req, res) {
+      Resource.getAll(function(err, docs) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(200).send(docs); }
+      });
+    });
+
+    app.use(bodyParser.json());
+
+    app.post('/cards', function(req, res) {
+      var hash = { name: 'default card name' };
+      Resource.create(hash, function(err, doc) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(201).send(doc._doc); }
+      });
+    });
+
+    app.put('/cards/:id', function(req, res) {
+      Resource.save(req.params.id, req.body, function(err, doc) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(200).send('Updated board ' + doc._doc._id); }
+      });
+    });
+
+    app.delete('/cards/:id', function(req, res) {
+      Resource.drop(req.params.id, function(err) {
+        if (err) { res.status(500).send('Database Error'); }
+        else { res.status(200).send('Dropped resource ' + req.params.id); }
+      });
+    });
+  });
+
+var port = 3000;
+app.listen(port, function() {
+  console.log('Listening on :' + port + '.');
+});
