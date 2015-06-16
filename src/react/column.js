aa12d654b14539013808ce5156f6835ad36196e3
Nick Peters <nickpeters@gmail.com>
2015-06-16 09:42:17 -0700

    Renamed property card 'id' to 'key'

--- src/react/column.js
+++ src/react/column.js
@@ -11,7 +11,7 @@ var Column = React.createClass({
   },
 
   componentDidMount: function() {
-    $.get('/column/' + this.props.id, function(data, status) {
+    $.get('/column/' + this.props.key, function(data, status) {
       this.setState({
         name: data.name,
         cards: data.cards
@@ -22,7 +22,7 @@ var Column = React.createClass({
   render: function() {
 
     var cardList = this.state.cards(function(card) {
-      return <Card id={card} />
+      return <Card key={card} />
     });
 
     return (
