d51aea458f186544777a0d35658d2ed0654e0b04
Jon Ringvald <jvald8@gmail.com>
2015-06-16 11:07:25 -0700

    commiting ma boawd list

--- src/react/boardList.js
+++ src/react/boardList.js
@@ -1,4 +1,4 @@
-var mockBoards = [{key:1, name: 'firstBoard'}];
+var mockBoards = [{key:1, name: 'firstBoard'}, {key:2, name: 'secondBoard'}];
 
 var React = require('react');
 
@@ -7,24 +7,23 @@ var $ = require('jquery');
 var BoardList = React.createClass({
   getInitialState: function() {
     return {
-      key: '',
-      name: '',
       boards: []
     }
   },
 
   componentDidMount: function() {
-    $.get('/boards/', function(err, data) {
+    var _this = this;
+    $.get(/*'/boards/'*/null, function(err, data) {
       if(err) {console.log('theres been an error')};
-      this.setState({
-        boards: data.boards
+      _this.setState({
+        boards: mockBoards
       })
     })
   },
 
   render: function() {
-    var boardList = this.state.boards(function(board) {
-      return <Board key={board.key} />
+    var boardList = this.state.boards.map(function(board) {
+      return <li><a href={'/boards/' + board.key}>{board.name}</a></li>
     });
 
     return (
@@ -32,25 +31,7 @@ var BoardList = React.createClass({
         {boardList}
       </div>
     )
-
-    /*return (
-      <main>
-        <a href={'/boards/' + mockBoards[0].key}>{mockBoards[0].name}</a>
-      </main>
-    )*/
   }
-  // get token from login service and set state to token
-  /*render: function() {
-    if(this.props.token === '') {
-      <p>Please login</p>
-    } else {
-      <ul>
-      mockBoards.forEach(function(board) {
-        <li><a href={'/board/' + board.id}></a></li>
-      })
-      </ul>
-    }
-  }*/
 })
 
 module.exports = BoardList;
