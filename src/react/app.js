f97879566cefad5ef87c28dde71aac68588a4e6c
Kendall Comey <kendall.comey@gmail.com>
2015-06-15 20:08:15 -0700

    Add if/else to app.js for login

--- src/react/app.js
+++ src/react/app.js
@@ -1,14 +1,24 @@
 var React = require('react');
 var User = require('./user');
 var Header = require('./header');
+var ShowBoards = require('./showBoards');
 
 var App = React.createClass({
+  // TODO: Hookup the check to see if user is authenticated
   render: function() {
+    var loggedIn = false;
+    var pageToShow;
+    if (loggedIn) {
+      pageToShow = <ShowBoards />
+    }
+    else {
+      pageToShow = <User />
+    }
     return (
       <main>
         <Header />
         <h1>Welcome to Yelo</h1>
-        <User />
+        {pageToShow}
       </main>
     );
   }
---
+++ src/react/showBoards.js
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var ShowBoards = React.createClass({
+  render: function(){
+    return (
+      <div>
+       Display the board list here
+      </div>
+    )
+  }
+});
+
+module.exports = ShowBoards;
+
+
+
+
+
+
