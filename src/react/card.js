25affd5a680cbb09af04951d2b267931cf84fd51
Daniel Esqueda <dan.a.esqueda@gmail.com>
2015-06-17 14:43:05 -0700

    add color div's

--- src/react/card.js
+++ src/react/card.js
@@ -9,7 +9,11 @@ var Card = React.createClass({
       colors: [],
       comments: [],
       users: [],
-      editView: false
+      editView: false,
+      contentButton: 'Save',
+      contentView: <textarea name='content'
+                             value={this.props.content}
+                             onChange={this.updateContent}/>
     };
   },
 
@@ -31,6 +35,28 @@ var Card = React.createClass({
     });
   },
 
+<<<<<<< HEAD
+=======
+  handleAddComment: function(e) {
+    e.preventDefault();
+    var newComment = this.refs.newComment.findDOMNode().value;
+    this.setState({
+      comments: this.state.comments.concat([newComment])
+    });
+    this.refs.newComment.getDOMNode().value = '';
+  },
+
+  handleColors: function(e) {
+
+    this.setState({
+      colors: this.state.colors.concat(['blue'])
+    });
+    console.log(e.target.class);
+    console.log(e.target.className);
+
+  },
+
+>>>>>>> add color div's
   componentDidMount: function() {
     $.get('/cards/' + this.props._id, function(data, status) {
       this.setState({
@@ -41,26 +67,73 @@ var Card = React.createClass({
         users: data.users
       });
     }.bind(this));
+<<<<<<< HEAD
+=======
+  },
+
+  toggleContent: function(e) {
+    e.preventDefault();
+    if (this.state.contentButton === 'Save') {
+
+      this.setState({
+        contentView: this.state.content,
+        contentButton: 'Edit'
+      });
+    } else {
+      this.setState({
+        contentView: <textarea name='content'
+                               defaultValue={this.state.content}
+                               value={this.props.content}
+                               onChange={this.updateContent}/>,
+        contentButton: 'Save'
+      });
+    }
+>>>>>>> add color div's
   },
 
   render: function() {
     var view;
     var buttonName;
+    var light;
+    var setColors = ['blue', 'green', 'red', 'yellow'];
+
+    var clickColors = setColors.map(function(setColor) {
+      return <div onClick={this.handleColors}
+                  refs={setColor}
+                  className={setColor}>{setColor}</div>
+    });
+
+    var coms = this.state.comments.map(function(comment) {
+      return <p>{comment}</p>
+    });
+    var colors = this.state.colors.map(function(color) {
+      return <div name={color} className='color-box'></div>
+    });
+    var users = this.state.users.map(function(user) {
+      return <div className={user}>{user}</div>
+    });
     if (this.state.editView) {
       view = <form>
         Enter Card Name:
         <input type='text'
                value={this.props.name}
                onChange={this.updateName} />
-        <textarea name='content' value={this.props.content} onChange={this.updateContent}/>
-        <textarea name='comments' value={this.props.comments} />
+        Card Content:
+        {this.state.contentView}
+        <button onClick={this.toggleContent}>{this.state.contentButton}</button>
+        Colors: {clickColors}
+        Comments:
+        <textarea name='comments' value={this.props.comments} ref='newComment' />
+        <button onClick={this.handleAddComment}>Add</button>
+        {coms}
       </form>;
       buttonName = 'Done';
     } else {
       view = <div>
         <h3>{this.state.name}</h3>
-        <p>{this.state.content}</p>
-        <p>{this.state.comments.length}</p>
+        {colors}
+        <p>{this.state.comments.length} comments</p>
+        {users}
       </div>
       buttonName = 'Edit';
     }
@@ -74,3 +147,4 @@ var Card = React.createClass({
 });
 
 module.exports = Card;
+
