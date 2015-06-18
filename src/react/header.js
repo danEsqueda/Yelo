c47c574c1dc4669983dc6495d064cf312f3c3ff3
Jon Ringvald <jvald8@gmail.com>
2015-06-18 10:48:47 -0700

    Modify header

--- src/react/header.js
+++ src/react/header.js
@@ -3,8 +3,10 @@ var React = require('react');
 var Header = React.createClass({
   render: function() {
     return (
-      <div>
-        <h3>Put HEADER here</h3>
+      <div id="header">
+        <div id="boards">{this.props.boards}</div>
+        <div id="headline">Yelo</div>
+        <div id="user">{this.props.currentUser}</div>
       </div>
     );
   }
